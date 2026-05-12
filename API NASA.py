92ee5eb84824b7c41864f397ad561bbd9a9e8beb
EmanuelEliud <emanuel.velazquezs@uanl.edu.mx>
2026-05-12 15:43:39 -0600

    Ahora si descarga la data

--- API NASA.py
+++ API NASA.py
@@ -1,7 +1,13 @@
 import requests
-import matplotlib.pyplot as plt
+import json
 
 API_KEY = "GGzdd1Vm4npty4LFHS5jwWtyYbcdALf67IjpcvEu"
 
-url = "https://api.nasa.gov/neo/rest/v1/feed?api_key={API_KEY}"
+url = f"https://api.nasa.gov/neo/rest/v1/feed?api_key={API_KEY}"
 
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    print(json.dumps(data, indent=4))
